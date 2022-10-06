59dfb4462599e9180cc060596d274df916c2ca5c
Ray-Shakex <bshakex@gmail.com>
2022-10-06 12:54:47 +0200

    hero IO

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ links.forEach(link => {
 // HERO INTERSECTION OBSERVER
 const hero = document.querySelector('.hero');
 const heroOptions = {
-    rootMargin: '-20% 0px 0px 0px',
+    rootMargin: '-90% 0px 0px 0px',
 }
 const heroObserver = new IntersectionObserver(
     function( entries, heroObserver) {
